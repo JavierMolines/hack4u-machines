0b6d1f691a2041615943a86d1cce2e10e8d47114
JavierMolines <eljavimolines@gmail.com>
2022-10-31 00:57:26 -0300

    Add label from total machines found in search

--- components/ContainerMachines/styles.tsx
+++ components/ContainerMachines/styles.tsx
@@ -1,8 +1,14 @@
 import styled from "styled-components"
 
 export const Container = styled.main`
-  margin: 1rem 2rem 5rem 2rem;
+  margin: 0rem 2rem 5rem 2rem;
   padding: 1rem;
   width: 99.5%;
   background-color: var(--colorGrayDark);
+`
+
+export const LabelFound = styled.p`
+  padding: 0rem 0.5rem;
+  width: 99.5%;
+  text-align: left;
 `
