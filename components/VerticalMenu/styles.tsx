e7bbd5ed1a39b811dbc37ee57c0864b3b9041a7e
JavierMolines <eljavimolines@gmail.com>
2022-11-15 02:32:06 -0300

    Update design VerticalMenu

--- components/VerticalMenu/styles.tsx
+++ components/VerticalMenu/styles.tsx
@@ -1,3 +1,21 @@
 import styled from "styled-components"
 
-export const Container = styled.div``
+export const FixedContainer = styled.div`
+  z-index: 10;
+  cursor: pointer;
+  position: fixed;
+  top: 1rem;
+  right: 1rem;
+`
+
+export const ContainerContent = styled.div`
+  z-index: 2;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #b01215df;
+`
