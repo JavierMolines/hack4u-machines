e6b2b7297dcc8eeeebc73f65d8cc6cdfd57160d2
JavierMolines <eljavimolines@gmail.com>
2022-12-07 14:36:46 -0300

    Add new spinner

--- components/Loading/styles.tsx
+++ components/Loading/styles.tsx
@@ -6,3 +6,29 @@ export const Container = styled.div`
   align-items: center;
   height: 50vh;
 `
+
+export const Spinner = styled.div`
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+
+  ::after {
+    content: " ";
+    display: block;
+    width: 64px;
+    height: 64px;
+    border-radius: 50%;
+    border: 6px solid white;
+    border-color: white transparent white transparent;
+    animation: rotation 1.5s linear infinite;
+
+    @keyframes rotation {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  }
+`
