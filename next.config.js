49128c24ec15fde9a541a2f8adc9af73b49809ee
JavierMolines <eljavimolines@gmail.com>
2023-12-03 03:26:12 -0300

    Refactor babel-style-component use / delete unused files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    styledComponents: {
+      ssr: true,
+      displayName: true,
+    },
+  },
 }
 
 module.exports = nextConfig
