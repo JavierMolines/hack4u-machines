68343fd60789355574eb35587a5732ffa3eb3309
Javier Molines <eljavimolines@gmail.com>
2024-04-25 08:18:30 -0300

    Add custom font to design web

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
+import { Roboto } from "next/font/google";
 import { MachinesProvider } from "../context/MachineContent";
 import "../styles/globals.css";
+
+const inter = Roboto({
+	subsets: ["latin"],
+	weight: "300",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<MachinesProvider>
-			<Component {...pageProps} />
+			<main className={inter.className}>
+				<Component {...pageProps} />
+			</main>
 			<Analytics />
 		</MachinesProvider>
 	);
