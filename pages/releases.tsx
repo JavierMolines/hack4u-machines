722e06bef137e55d6c15931a581a3d2f45e45193
JavierMolines <eljavimolines@gmail.com>
2024-12-27 10:05:04 -0300

    fix: releases page design

--- pages/releases.tsx
+++ pages/releases.tsx
@@ -7,18 +7,18 @@ const Shortcuts: NextPage = () => {
 	return (
 		<PageLayout title="Machines - Releases">
 			<BlogPost $maxWidth="85ch">
-				<h1 className="text-center">Releases - Changelog</h1>
+				<h1 className="text-center text-3xl py-4">Releases - Changelog</h1>
 
 				{PROJECT_VERSIONS.map(({ version, changes, date }, _) => (
 					<section key={version + date + 1}>
-						<h1 id={version}>
+						<h1 className="text-2xl py-3" id={version}>
 							{version} - [{date}]
 						</h1>
 
-						<ul className="pl-2">
+						<ul className="py-2 pl-2 list-disc">
 							{changes.map((change, _) => (
 								<li
-									className="py-2 text-base list-item text-left"
+									className="py-1 text-base list-item text-left"
 									key={`${change}`}
 								>
 									{change}.
