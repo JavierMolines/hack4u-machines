b26eed9651ea996c3c335bf44c2697a1d1591944
JavierMolines <eljavimolines@gmail.com>
2024-12-28 04:50:42 -0300

    fix: filter design

--- components/Filter/index.tsx
+++ components/Filter/index.tsx
@@ -1,6 +1,5 @@
 import { type FormEvent, useContext, useEffect, useRef, useState } from "react";
 import { MachinesContext } from "../../context/MachineContent";
-import { useDevice } from "../../hooks/useDevice";
 import { filterExact, sortHight, storagesKeys } from "../../utils/definition";
 import { handlerOverflowVertical } from "../../utils/domMethods";
 import { getStorage, removeStorage, setStorage } from "../../utils/storage";
@@ -10,9 +9,6 @@ import type { IFilter } from "./types";
 
 const Filter: React.FC<IFilter> = ({ callbackShowMachines }) => {
 	const dimension: number = 25;
-	const inputPlaceHolder =
-		"Filter for anything you want: (Ex: HackTheBox Medium Active Directory OSEP Kerberos)";
-	const { isMobile } = useDevice();
 	const contextMachine = useContext(MachinesContext);
 	const inputFilter = useRef<HTMLInputElement>(null);
 	const [showAdvancedFilter, setShowAdvancedFilter] = useState<boolean>(false);
@@ -22,6 +18,16 @@ const Filter: React.FC<IFilter> = ({ callbackShowMachines }) => {
 		try {
 			setTotalSearchOption(getStorage(storagesKeys.filterOption).length);
 		} catch (error) {}
+	};
+
+	const generateCountSelectOptions = () => {
+		if (totalSearchOption === 0) return <></>;
+
+		return (
+			<span className="absolute bottom-1 right-0 text-[#ffd700] text-center text-[15px] p-0">
+				{totalSearchOption}
+			</span>
+		);
 	};
 
 	const filterOnClick = () => {
@@ -95,35 +101,38 @@ const Filter: React.FC<IFilter> = ({ callbackShowMachines }) => {
 	return (
 		<section className="w-full">
 			<form
-				className="grid grid-cols-1 bg-gray-800 rounded-[0.5rem]"
 				autoComplete="off"
 				onSubmit={onSubmit}
+				className="grid grid-cols-2 lg:grid-cols-[5%_80%_15%] bg-[#242525]"
 			>
 				<div
 					id="machinesButtonFilter"
 					onClick={filterOnClick}
-					className="w-full flex justify-center items-center bg-red-800 rounded-tl-[0.5rem] rounded-bl-[0.5rem] cursor-pointer"
+					className="order-0 flex justify-center cursor-pointer rounded-l-lg col-span-1 bg-[#b01214] hover:bg-[#8a0f11] text-white text-center p-2"
 				>
 					<Icon src="/filter.svg" dimension={dimension} />
 				</div>
 
-				<div className="w-full flex justify-center items-center">
-					<Icon src="/search.svg" dimension={dimension} />
+				<div className="order-3 lg:order-2 col-span-2 lg:col-span-1 grid grid-cols-[10%_1fr] lg:grid-cols-[6%_1fr]">
+					<div className="bg-transparent flex justify-center col-span-1 bg-blue-500 text-white text-center p-2 relative">
+						<Icon src="/search.svg" dimension={dimension} />
+						{generateCountSelectOptions()}
+					</div>
+
+					<input
+						type="text"
+						id="machinesButtonSearch"
+						aria-autocomplete="none"
+						autoComplete="off"
+						ref={inputFilter}
+						placeholder="Filter for anything you want: (Ex: HackTheBox Medium Active Directory OSEP Kerberos)"
+						className="text-lg outline-none bg-transparent col-span-1 bg-green-500 text-white p-2"
+					/>
 				</div>
 
-				<input
-					type="text"
-					className="text-[1.2rem] py-[0.75rem] px-[0.25rem] border-0 outline-none bg-transparent text-white placeholder-gray-400"
-					id="machinesButtonSearch"
-					aria-autocomplete="none"
-					autoComplete="off"
-					ref={inputFilter}
-					placeholder={inputPlaceHolder}
-				/>
-
 				<button
-					type="button"
-					className="rounded-tr-[0.5rem] rounded-br-[0.5rem] bg-red-800 text-white border-0 outline-none text-[1.1rem] py-[0.5rem] px-0 cursor-pointer"
+					type="submit"
+					className="order-2 lg:order-6 rounded-r-lg col-span-1 bg-[#b01214] hover:bg-[#8a0f11] text-white text-lg p-2"
 				>
 					Search
 				</button>
